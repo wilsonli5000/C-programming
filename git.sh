9d919e25f3ceb07023b6a5a23acf4a482a02b066
wilsonli5000 <wilsonli5000@gmail.com>
2016-03-04 10:33:46 -0800

    Commit by Wilson Li

--- git.sh
+++ git.sh
@@ -1,7 +1,10 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 
-#commit
-git commit -a commit
+#add all the files
+git add -A
+
+#commit with a defult message
+git commit -m "Commit by Wilson Li"
 
 #push 
 git push origin master
